66ea5aab6d9ab38455aa5b0aadd70d4e45cc2065
Eason4869 <53282468+Eason4869@users.noreply.github.com>
2026-09-24 16:30:12 +0800

    chore: bump 1.2.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.yusheng.quota"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.2.23"
+        versionCode = 24
+        versionName = "1.2.24"
         resourceConfigurations += listOf("en", "zh-rCN")
     }
 
@@ -133,4 +133,5 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
+
 
